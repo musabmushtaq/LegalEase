f9735ee2dfd093f435edcd5047f0a859bb55792d
musabmushtaq <musabmushtaq952@gmail.com>
2026-05-16 18:07:45 +0500

    UI: temporray chat reminder updated to only show on a new page

--- app/lib/screens/chat_screen.dart
+++ app/lib/screens/chat_screen.dart
@@ -394,7 +394,7 @@ class _ChatScreenState extends State<ChatScreen> {
                       : const SizedBox.shrink(),
                   ),
 
-                  if (_chatService.isTemporaryChat)
+                  if (_chatService.isTemporaryChat && _chatService.currentMessages.isEmpty)
                     Padding(
                       padding: const EdgeInsets.only(bottom: 8.0),
                       child: Text(
